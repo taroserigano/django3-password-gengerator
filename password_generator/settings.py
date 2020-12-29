e52ee51e29ebcc95983d9803c782b49807091754
Taro Serigano <taroserigano@gmail.com>
2020-12-29 17:38:22 +0100

    Update settings.py

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -16,11 +16,9 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r2lzqfd)7+5#2=gu%h1i!a4o1q9s%s8z7(@$*3kf-m#upvgu^j'
+SECRET_KEY = 'CANT_TELL_U'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
